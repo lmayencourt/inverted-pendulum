f6c94ca97583feda1cccae4bf49d6d473aea1395
Louis Mayencourt <louis.mayencourt@gmail.com>
2024-07-07 15:44:29 +0200

    Simulate a anchor point with a rope joint attached to it

--- src/simulator.rs
+++ src/simulator.rs
@@ -5,17 +5,25 @@
  */
 
 use bevy::prelude::*;
+use bevy_rapier2d::prelude::*;
 
 pub struct SimulatorPlugin;
 
 /// Cart sprite size
-const PENDULUM_CART_WIDTH: f32 = 40.0;
-const PENDULUM_CART_HEIGHT: f32 = PENDULUM_CART_WIDTH * 1.618;
+const PENDULUM_CART_HEIGHT: f32 = 40.0;
+const PENDULUM_CART_WIDTH: f32 = PENDULUM_CART_HEIGHT * 1.618;
+
+/// Pendulum size
+const PENDULUM_WIDTH: f32 = 10.0;
+const PENDULUM_HEIGHT: f32 = 100.0;
 
 impl Plugin for SimulatorPlugin {
     fn build(&self, app: &mut App) {
+        app.add_plugins(RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(100.0));
+        app.add_plugins(RapierDebugRenderPlugin::default());
         app.add_systems(Startup, setup_graphics);
         app.add_systems(Startup, setup_physics);
+        app.add_systems(Update, bevy::window::close_on_esc);
     }
 }
 
@@ -24,12 +32,34 @@ fn setup_graphics(mut commands: Commands) {
 }
 
 fn setup_physics(mut commands: Commands) {
-    commands.spawn(SpriteBundle{
+    let parent = commands.spawn(SpriteBundle{
         transform: Transform {
             translation: Vec3::new(0.0, 0.0, 0.0),
-            scale: Vec3::new(PENDULUM_CART_HEIGHT, PENDULUM_CART_WIDTH, 1.0),
+            scale: Vec3::new(PENDULUM_CART_WIDTH, PENDULUM_CART_HEIGHT, 1.0),
             ..default()
         },
         ..default()
-    });
+    })
+    .insert(RigidBody::KinematicVelocityBased)
+    .insert(Collider::cuboid(0.5, 0.5))
+    .id();
+
+    // let joint = RevoluteJointBuilder::new().local_anchor1(Vec2::new(0.0, 0.0));
+    let mass_size = 10.0;
+    let rope_length = 100.0;
+    let joint = RopeJointBuilder::new(rope_length).local_anchor2(Vec2::new(rope_length, 0.0));
+    commands
+            .spawn((
+                SpriteBundle{
+                    transform: Transform {
+                        translation: Vec3::new(PENDULUM_CART_WIDTH/2.0+1.0, rope_length*2.0, 0.0),
+                        scale: Vec3::new(mass_size, mass_size, 1.0),
+                        ..default()
+                    },
+                    ..default()
+                },
+                RigidBody::Dynamic,
+                Collider::cuboid(0.5, 0.5),
+            ))
+            .insert(ImpulseJoint::new(parent, joint));
 }
